00d9ad9993d6c17eacad59e48bf06f9135483f8c
Nishanshah9 <142131567+Nishanshah54@users.noreply.github.com>
2025-08-13 20:41:31 +0545

    day 1

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Day 1
+// let number=prompt('Enter integer Number');
+// console.log("Enter number is",number);
+
+// alert("warring!!");
+const string1 = "A string primitive";
+const string2 = 'Also a string primitive';
+const string3 = `Yet another string primitive`;
+// console.log(string1.toUpperCase());
+// console.log(string1.toLowerCase());
+// console.log(string1.length);
+// console.log(string1.length);
+
+// about data type
+let num=12;
+console.log(num);
+console.log( num,"is a datatype",typeof(num));
+
+// string
+let name1="saktiman";
+console.log(name1);
+console.log(name1,"is",typeof(name1),"Datatype");
+
+// Day 2
