058a15636445ceae0c79edb1f3ad803dafd961e9
Nishanshah9 <142131567+Nishanshah54@users.noreply.github.com>
2025-08-15 22:17:53 +0545

    added day2

---
+++ Day2/script.js
@@ -0,0 +1,68 @@
+//  Print all even numbers from 0 to 100
+            // const N=prompt("To Print any number for 0 to N number Enter N Integer");
+            // let i=1;
+
+            // 1. using for loop
+                // for( i; i<=N;i++)
+                // {
+                //     if(i%2==0)
+                //     {
+                //     console.log(i,"is Even Number");
+                //     }
+            // }
+
+            // 2. do while loop
+                    // do{
+                    //     if(i%2==0)
+                    //     {
+
+                    //         console.log(i,"is Even Number");
+                    //     }
+                    //     i++;
+                    // }
+                    // while(i<=N);
+
+            // 3. while loop
+                // while(i<=N)
+                // {
+                //    if(i%2==0)
+                //             {
+                //                 console.log(i,"is Even Number");
+                //             }
+                //             i++;  
+                // }                                                                                
+// 4. for in loop
+    // let string="Test For of Loop";
+    // // let vowel=['e']
+    // let count =0;
+    // for(let val of string)
+    // {
+    //     if(val=='a' || val == 'e' || val=="i" || val=="o" || val=="u" )
+    //     {
+    //     count++;
+    //     console.log(val);
+    //     }       
+
+        
+    // }
+    //  console.log("Number of vowel in given",string,"is",count);
+
+// 5. for of loop
+const colors=['red','bule','yellow','orange','white','pink','green'];
+
+for (let color of colors)
+    {
+        if(color=='red')
+        console.log(color)
+    }
+
+
+
+// const array=[19,10,12,34,45,54,32,45,65];
+
+// const found =array.find(element=>element===10);
+// console.log(found);
+    
+
+
+
